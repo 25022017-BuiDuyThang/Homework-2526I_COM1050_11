b500f10b5cfc53147bc7699a2235397bc271352c
25022017-BuiDuyThang <25022017@vnu.edu.vn>
2025-10-02 13:30:12 +0700

    vcl

--- ThucHanh3/Bai15Rouding.py
+++ ThucHanh3/Bai15Rouding.py
@@ -1,7 +1,6 @@
 a = float(input("Nhập số thực: "))
 phannguyen = int(a)
 
-# Làm tròn xuống (floor)
 if a >= 0: 
     val3 = phannguyen
 else: 
@@ -10,7 +9,6 @@ else:
     else: 
         val3 = phannguyen - 1
 
-# Làm tròn lên (ceil)
 if a >= 0:
     if a == phannguyen: 
         val1 = phannguyen
@@ -19,9 +17,7 @@ if a >= 0:
 else: 
     val1 = phannguyen
 
-# Làm tròn gần nhất (round)
 digit = a - phannguyen
-
 if digit >= 0.5: 
     val2 = phannguyen + 1
 elif digit <= -0.5: 
