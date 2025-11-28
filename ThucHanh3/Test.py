14f85c4a4b10f8994b8a56af990b2e75659ce1a6
25022017-BuiDuyThang <25022017@vnu.edu.vn>
2025-11-28 08:49:55 +0700

    u

--- ThucHanh3/Test.py
+++ ThucHanh3/Test.py
@@ -1,4 +1,19 @@
-import math
 
-n = int(input())
-print(n)
+def check(c):
+    return c >='1' and c <='9'
+
+
+s = str(input())
+
+res = 0
+i = 0
+while i < len(s):
+    if check(s[i]):
+        temp = 0
+        while i < len(s) and check(s[i]):
+            temp = temp * 10 + int(s[i])
+            i += 1
+        res += temp
+    else:
+        i += 1
+print(res)
